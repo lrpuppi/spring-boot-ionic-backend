9b0554f26bdaf63e3323b3bdef4ea92bac3e20be
lrpuppi <lrpuppi@gmail.com>
2019-01-08 12:16:46 -0200

    Autorizando endpoints para perfis especificos

--- src/main/java/com/lrp/cursomc/resources/PedidoResource.java
+++ src/main/java/com/lrp/cursomc/resources/PedidoResource.java
@@ -13,9 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.lrp.cursomc.domain.Categoria;
 import com.lrp.cursomc.domain.Pedido;
-import com.lrp.cursomc.dto.CategoriaDTO;
 import com.lrp.cursomc.services.PedidoService;
 
 @RestController
