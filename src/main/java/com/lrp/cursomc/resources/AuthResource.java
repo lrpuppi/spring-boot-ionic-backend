894127f0421a7f73f86c71326800dfd135ccaf40
lrpuppi <lrpuppi@gmail.com>
2019-01-08 14:25:54 -0200

    Esqueci a senha

--- src/main/java/com/lrp/cursomc/resources/AuthResource.java
+++ src/main/java/com/lrp/cursomc/resources/AuthResource.java
@@ -1,15 +1,19 @@
 package com.lrp.cursomc.resources;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.lrp.cursomc.dto.EmailDTO;
 import com.lrp.cursomc.security.JWTUtil;
 import com.lrp.cursomc.security.UserSS;
+import com.lrp.cursomc.services.AuthService;
 import com.lrp.cursomc.services.UserService;
 
 @RestController
@@ -17,12 +21,21 @@ import com.lrp.cursomc.services.UserService;
 public class AuthResource {
 	@Autowired
 	private JWTUtil jwtUtil;
+	
+	@Autowired
+	private AuthService service;
 
 	@RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
 	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
 		UserSS user = UserService.authenticated();
 		String token = jwtUtil.generateToken(user.getUsername());
 		response.addHeader("Authorization", "Bearer " + token);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value = "/forgot", method = RequestMethod.POST)
+	public ResponseEntity<Void> forgot(@Valid @RequestBody EmailDTO objDto) {
+		service.sendNewPassword(objDto.getEmail());
 		return ResponseEntity.noContent().build();
 	}
 }
--- src/main/java/com/lrp/cursomc/services/EmailService.java
+++ src/main/java/com/lrp/cursomc/services/EmailService.java
@@ -2,6 +2,7 @@ package com.lrp.cursomc.services;
 
 import org.springframework.mail.SimpleMailMessage;
 
+import com.lrp.cursomc.domain.Cliente;
 import com.lrp.cursomc.domain.Pedido;
 
 public interface EmailService {
@@ -9,5 +10,7 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 
 	void sendEmail(SimpleMailMessage msg);
+
+	void sendNewPasswordEmail(Cliente cliente, String newPass);
 	
 }
