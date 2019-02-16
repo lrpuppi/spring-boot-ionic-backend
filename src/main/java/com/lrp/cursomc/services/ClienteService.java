796905748aa7a350837418badebd6e2c2aa17b80
lrpuppi <lrpuppi@gmail.com>
2019-02-16 11:26:19 -0200

    Endpoint para buscar cliente por email

--- src/main/java/com/lrp/cursomc/services/ClienteService.java
+++ src/main/java/com/lrp/cursomc/services/ClienteService.java
@@ -90,6 +90,21 @@ public class ClienteService {
 			throw new DataIntegrityException("Não é possível excluir porque há pedidos relacionados");
 		}
 	}
+	
+	public Cliente findByEmail(String email) {
+
+		UserSS user = UserService.authenticated();
+		if (user == null || !user.hasRole(Perfil.ADMIN) && !email.equals(user.getUsername())) {
+			throw new AuthorizationException("Acesso negado");
+		}
+
+		Cliente obj = repo.findByEmail(email);
+		if (obj == null) {
+			throw new ObjectNotFoundException(
+					"Objeto não encontrado! Id: " + user.getId() + ", Tipo: " + Cliente.class.getName());
+		}
+		return obj;
+	}
 
 	public List<Cliente> findAll() {
 		return repo.findAll();
